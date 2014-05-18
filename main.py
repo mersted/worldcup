00d445883ea49972c26f9d8b2259c40e2b5b18a8
Matt E <mersted@gatech.edu>
2014-05-17 22:04:46 -0700

    minor changes

--- main.py
+++ main.py
@@ -7,19 +7,7 @@ class WorldCup:
       self.db = self.m.worldcup
 
     def insert_game(self, game):
-      try:
-        self.db.games.insert(game)
-        doc = {
-              'home_team' : game['ht'],
-              'away_team' : game['at'],
-              'stats' : {
-                  'home_goals' : game['hg'],
-                  'away_goals' : game['ag'],
-                  'home_yellow' : game['hy'],
-                  'away_yellow' : game['ay'],
-                  'home_red' : game['hr'],
-                  'away_red' : game['ar']
-              },
-              'stadium' : game['stadium'],
-              'round' : game['round']
-              }
+        try:
+            self.db.games.insert(game)
+        except:
+            return
