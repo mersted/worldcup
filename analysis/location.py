bde0fc1028487596324a86f620c9bd4066cc3d1a
Matt E <mersted@gatech.edu>
2014-05-25 21:17:38 -0700

    improved location analysis

--- analysis/location.py
+++ analysis/location.py
@@ -6,11 +6,28 @@ connection = pymongo.MongoClient("localhost", 27017)
 db = connection.worldcup
 tweets = db.tweets
 
-def main(xmin, xmax, ymin, ymax):
+def tweets_by_country(country):
 
-    query = {}
+    arr = countries[country]
+    xmin = arr[0]
+    xmax = arr[1]
+    ymin = arr[2]
+    ymax = arr[3]
+
+    query = {'location.x' : {'$gte' : xmin, '$lte' : xmax},
+              'location.y' : {'$gte' : ymin, '$lte' : ymax}}
 
     try:
-        cursor = tweets.find(query)
+        total = tweets.count(query)
     except:
         print("Unexpected error:", sys.exc_info()[0])
+
+    return total
+
+# dictionary of countries with max and min
+# longitude and latitude
+countries = {}
+# countries['Brazil'] = [xmin, xmax, ymin, ymax]
+# countries['Croatia'] = [xmin, xmax, ymin, ymax]
+
+result = tweets_by_country('Brazil')
