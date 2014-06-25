577bbcf19d0ccfd3bf487129eaa892d5d54a36a2
Matt E <mersted@gatech.edu>
2014-06-24 23:41:03 -0700

    analysis of usa v gha game

--- analysis/frequency.py
+++ analysis/frequency.py
@@ -16,24 +16,27 @@ def total_tweets_interval(start, end):
 
     query = {'created_at' : {'$gte' : millistart, '$lte' : milliend}}
     try:
-      results = db.tweets2.find(query)
-      total = results.count()
-      return total
-    except:
-      print("Unexpected error:", sys.exc_info()[0])
-      return 0
-
-def keyword_tweets_total(word):
-
-    query = {'$text' : {'$search' : word}}
-
-    try:
       results = db.tweets.find(query)
       total = results.count()
       return total
     except:
       print("Unexpected error:", sys.exc_info()[0])
       return 0
+
+def keyword_tweets_total(words):
+
+    buckets_tot = {}
+    print(type(words))
+    for word in words:
+        query = {'$text' : {'$search' : word}}
+        try:
+          results = db.tweets2.find(query)
+          total = results.count()
+          buckets_tot[word] = total
+        except:
+          print("Unexpected error:", sys.exc_info()[0])
+
+    return buckets_tot
 
 def keyword_tweets_interval(start, end, word):
 
@@ -101,28 +104,27 @@ def match_time_intervals(day, month, gametime):
 
     hour = int(gametime[:2])
     minute = int(gametime[3:5])
-    words = ["neymar", "fred", "ref", "oscar", "croatia", "brasil", "modric"]
+    words = ["dempsey", "goal", "ayew", "brooks", "usa", "ghana", "bradley"]
     for word in words:
         tot = keyword_tweets_total(word)
         buckets_tot[word] = tot
-        # for m in range(25):
-        #     if minute < 55:
-        #         x, y = generate_time_interval(day, month, hour, minute)
-        #         minute += 5
-        #     else:
-        #         x, y = generate_time_interval(day, month, hour, minute)
-        #         if hour < 23:
-        #             hour += 1
-        #         else:
-        #             hour = 0
-        #         minute = 5 - (60 - minute)
-        #
-        #     #tot = total_tweets_interval(x, y)
-        #     tot = keyword_tweets_interval(x, y, word)
-        #     if word in buckets_int:
-        #       buckets_int[word] += (x, tot)
-        #     else:
-        #       buckets_int[word] = [(x, tot)]
+        for m in range(25):
+            if minute < 55:
+                x, y = generate_time_interval(day, month, hour, minute)
+                minute += 5
+            else:
+                x, y = generate_time_interval(day, month, hour, minute)
+                if hour < 23:
+                    hour += 1
+                else:
+                    hour = 0
+                minute = 5 - (60 - minute)
+
+            tot = keyword_tweets_interval(x, y, word)
+            if word in buckets_int:
+              buckets_int[word] += (x, tot)
+            else:
+              buckets_int[word] = [(x, tot)]
 
     return buckets_tot
 
@@ -139,10 +141,11 @@ def create_text_file(dict, filename):
     #     for tup in ls:
     #       f.write(tup[0] + "\t" + str(tup[1]) + "\n")
 
-# function calls for first game, Brasil v. Croatia
-results = match_time_intervals("12", "06", "19:52:00")
-create_text_file(results, "BRAvCRO_3.txt")
+words = ["dempsey", "goal", "ayew", "brooks", "usa", "ghana", "bradley"]
+results = keyword_tweets_total(words)
+create_text_file(results, "USAvGHA.txt")
+#results = match_time_intervals("12", "06", "19:52:00")
+#create_text_file(results, "BRAvCRO_3.txt")
 # function calls for first USA game, USA v. Ghana
 #results = match_time_intervals("16", "06", "22:52:00")
 #create_text_file(results, "USAvGHA.txt")
-# start = 1402602728
