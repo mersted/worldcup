28a972cb9ef5e004d78fd3847b8a56d26fb41f72
Matt E <mersted@gatech.edu>
2014-06-30 21:11:10 -1000

    prepare for usa v bel game

--- gather/track.py
+++ gather/track.py
@@ -6,7 +6,7 @@ import time
 
 
 if __name__ == '__main__':
-    words = "usa,america,germany,worldcup,worldcup2014,dempsey,bradley,muller,advance,goal,beatgermany"
+    words = "usa,america,belgium,worldcup,worldcup2014,dempsey,bradley,hazard,kompany"
     pipes = []
     for word in words.split(","):
         pipes.append(RedisPipe(word, period=15, db=1, ns="track:" + word))
